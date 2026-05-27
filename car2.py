05d9682143e42aa68ed52f0117fb4b3d0bea8213
Artem Markov <zeldeck@gmail.com>
2026-05-27 11:42:00 +0300

    Добавил вертолёты

--- car2.py
+++ car2.py
@@ -8,9 +8,9 @@ class Car:
 
     def start_engine(self, status):
         if status == 1:
-            return "завелась"
+            return "есть контакт"
         else:
-            return "не завелась"
+            return "всё плохо"
 
     def car_conclusions(self, final):
         if final == 2:
@@ -30,6 +30,15 @@ class FireTruck(Car):
         self.water_tank = water_tank
 
 
+class PoliceHelicopter(Car):
+
+    # В __init__ мы просим всё то же самое + объем цистерны с водой
+    def __init__(self, brand, country, year, condition, man):
+        # super() отправляет базовые свойства наверх в класс Car [Python]
+        super().__init__(brand, country, year, condition)
+        self.man = man
+
+
 car1 = Car(brand="BMW", country="ФРГ", year=2020, condition="Новая")
 car2 = Car(brand="Tesla", country="США", year=2023, condition="Использованная")
 car3 = Car(brand="Жигули", country="СССР", year=1989, condition="Говно")
@@ -44,7 +53,17 @@ fire_car3 = FireTruck(
     brand="Краз", country="РФ", year=2024, condition="Чуть новая", water_tank=200
 )
 
-print("Техобслуживание автомобилей")
+helicopter1 = PoliceHelicopter(
+    brand="Ми-8", country="РФ", year=2026, condition="только с завода", man=8
+)
+helicopter2 = PoliceHelicopter(
+    brand="Робинсон-2", country="США", year=2016, condition="Б/у", man=2
+)
+
+
+print("Открываем сервис")
+print("-" * 30)
+print("Проверяем наличие")
 print(" ")
 print("Проверяем описание машин")
 print(
@@ -68,15 +87,27 @@ print(
     f"Спецмашина 3: марка: {fire_car3.brand}, страна: {fire_car3.country}, год: {fire_car3.year}, состояние: {fire_car3.condition}, цистерна: {fire_car3.water_tank} л."
 )
 print(" ")
+print("Проверяем описание вертолётов")
+print(
+    f"Вертолёт 1: марка: {helicopter1.brand}, страна: {helicopter1.country}, год: {helicopter1.year}, состояние: {helicopter1.condition}, людей помещается: {helicopter1.man}."
+)
+print(
+    f"Вертолёт 2: марка: {helicopter2.brand}, страна: {helicopter2.country}, год: {helicopter2.year}, состояние: {helicopter2.condition}, людей помещается: {helicopter2.man}."
+)
+print(" ")
 print("Проверяем мотор")
-print(f"{car1.brand} {car1.start_engine(1)} с первого раза")
-print(f"{car2.brand} {car2.start_engine(1)} легко")
-print(f"{car3.brand}, ёбаный в рот, опять {car3.start_engine(0)}")
+print(f"{car1.brand}: {car1.start_engine(1)}")
+print(f"{car2.brand}: {car2.start_engine(1)}")
+print(f"{car3.brand}: ёбаный в рот, опять {car3.start_engine(0)}")
 print(" ")
 print("Проверяем мотор спецтехники")
-print(f"{fire_car1.brand} {fire_car1.start_engine(1)} с первого раза")
-print(f"{fire_car2.brand} {fire_car2.start_engine(1)} легко")
-print(f"{fire_car3.brand} опять {fire_car3.start_engine(0)}")
+print(f"{fire_car1.brand}: {fire_car1.start_engine(1)} ")
+print(f"{fire_car2.brand}: {fire_car2.start_engine(1)} ")
+print(f"{fire_car3.brand}: опять {fire_car3.start_engine(0)}")
+print(" ")
+print("Проверяем мотор вертолетов")
+print(f"{helicopter1.brand}: {helicopter1.start_engine(1)}")
+print(f"{helicopter2.brand}: {helicopter2.start_engine(1)}")
 print(" ")
 print("Выводы по машинам")
 print(f"моё мнение по {car1.brand}: {car1.car_conclusions(2)}")
